89b4efc7073a58e89cf5d73c210303668833abee
dlion4 <dantemilimo@gmail.com>
2025-07-13 18:30:24 +0300

    resolved the allowed host

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,6 +20,15 @@ if READ_DOT_ENV_FILE:
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", False)
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    ".localhost",
+    "31.220.58.182",
+    "pkenya.makelaw.ke",
+    ".makelaw.ke",
+    "127.0.0.1",
+]
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
