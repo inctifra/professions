2160f0409d7267f02482237fca63f6d4d31f5db5
dlion4 <dantemilimo@gmail.com>
2025-08-24 14:41:06 +0300

    removed the django celery email since it is not useful

--- config/settings/production.py
+++ config/settings/production.py
@@ -141,7 +141,6 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
 INSTALLED_APPS += ["anymail"]
-INSTALLED_APPS += ("djcelery_email",)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
